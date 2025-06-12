b630ccc63b2aa2e818148ecaad515929784f505a
Jashan <jashanjot381@gmail.com>
2025-06-11 22:12:48 -0400

    Created the base Employee class

--- EmpSysJashan/Employee.swift
+++ EmpSysJashan/Employee.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+class Employee {
+    var name: String
+    var id: Int
+
+    // Computed property for salary, returns 0 for the base class
+    var salary: Double {
+        return 0
+    }
+
+    init(name: String, id: Int) {
+        self.name = name
+        self.id = id
+    }
+
+    // Method to display basic employee details
+    func displayDetails() {
+        print("ID: \(id), Name: \(name)", terminator: "")
+    }
+}
+
+
---
+++ EmpSysJashan/Taxable.swift
@@ -0,0 +1,8 @@
+//
+//  Taxable.swift
+//  EmpSysJashan
+//
+//  Created by Jashan Kalsi on 2025-06-11.
+//
+
+import Foundation
